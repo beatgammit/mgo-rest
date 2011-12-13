31f74091b2fd96c3e19f7a20a62c3ddefe9e2246
Jameson Little <t.jameson.little@gmail.com>
2011-12-13 00:08:44 -0700

    DELETE and PUT implemented; added query param hexId to signal whether the passed docid is hex or a regular string

--- src/main.go
+++ src/main.go
@@ -5,10 +5,12 @@ import (
 	"launchpad.net/mgo"
 	"artichoke"
 	"net/http"
+	"net/url"
 	"encoding/json"
 	"strconv"
 	"fmt"
 	"strings"
+	"encoding/hex"
 )
 
 var session *mgo.Session
@@ -50,11 +52,42 @@ func genRoutes() []artichoke.Route {
 				s := session.New()
 				defer s.Close()
 
+				query := m["query"].(url.Values)
 				params := m["params"].(map[string]string)
+
+				c := session.DB(params["db"]).C(params["collection"])
+
+				var id interface{}
+				// default to hex if hexId isn't present
+				if query.Get("hexId") == "false" {
+					id = params["docid"]
+				} else {
+					str, err := hex.DecodeString(params["docid"])
+					if err != nil {
+						writeError(w, 500, "Unable to convert supplied string to hex format")
+						return true
+					} else if !bson.ObjectId(str).Valid() {
+						writeError(w, 400, "Invalid hex string. Maybe you meant to pass the query param hexId=false?")
+						return true
+					}
+
+					id = bson.ObjectId(str)
+				}
+
+				// we'll need this query later
+				q := c.Find(bson.M{"_id": id})
+				n, err := q.Count()
+				if err != nil {
+					writeError(w, 500, "Error counting results")
+					return true
+				} else if n == 0 {
+					writeError(w, 404, "Document not found")
+					return true
+				}
 
 				// we don't know the structure before-hand, but we know it's a JSON object
 				var out map[string]interface{}
-				err := session.DB(params["db"]).C(params["collection"]).Find(bson.M{"_id": bson.ObjectIdHex(params["docid"])}).One(&out)
+				err = q.One(&out)
 				if err != nil {
 					writeError(w, 500, "Error getting document by id")
 					return true
@@ -78,13 +111,14 @@ func genRoutes() []artichoke.Route {
 				s := session.New()
 				defer s.Close()
 
-				// get the collection the user specified
 				params := m["params"].(map[string]string)
+				query := m["query"].(url.Values)
+
+				// get the collection the user specified
 				c := session.DB(params["db"]).C(params["collection"])
 
 				// check for query params
-				js := m["query"]
-				if js != nil {
+				if len(query) > 0 {
 					// not yet implemented
 					writeError(w, 500, "Query params not supported yet")
 					return true
@@ -147,6 +181,187 @@ func genRoutes() []artichoke.Route {
 				return true
 			},
 		},
+
+		/* DELETE Requests */
+
+		artichoke.Route{
+			Method: "DELETE",
+			Pattern: "/:db/:collection/:docid",
+			Handler: func(w http.ResponseWriter, r *http.Request, m artichoke.Data) bool {
+				if m["bodyJson"] != nil {
+					writeError(w, 400, "DELETE requests on document ids takes no parameters, and thus should have no body or query parameters.")
+					return true
+				}
+
+				// create a new session per connection
+				s := session.New()
+				defer s.Close()
+
+				// get the params and query objects
+				query := m["query"].(url.Values)
+				params := m["params"].(map[string]string)
+
+				c := s.DB(params["db"]).C(params["collection"])
+
+				var id interface{}
+				if query.Get("hexId") == "false" {
+					id = params["docid"]
+				} else {
+					str, err := hex.DecodeString(params["docid"])
+					if err != nil {
+						writeError(w, 500, "Unable to convert supplied string to hex format")
+						return true
+					} else if !bson.ObjectId(str).Valid() {
+						writeError(w, 400, "Invalid hex string. Maybe you meant to pass the query param hexId=false?")
+						return true
+					}
+
+					id = bson.ObjectId(str)
+				}
+
+				obj := bson.M{"_id": id}
+
+				n, err := c.Find(obj).Count()
+				if err != nil {
+					writeError(w, 500, "Error communicating with database")
+					return true
+				}
+
+				if n == 0 {
+					writeError(w, 404, "The id provided was not found in this database. Either the document has been deleted already or it never existed.")
+					return true
+				}
+
+				err = c.Remove(obj)
+				if err != nil {
+					writeError(w, 500, "Error removing item from database")
+					return true
+				}
+
+				w.Header().Add("Content-Length", "0")
+				w.WriteHeader(204)
+				return true
+			},
+		},
+		artichoke.Route{
+			Method: "DELETE",
+			Pattern: "/:db/:collection",
+			Handler: func(w http.ResponseWriter, r *http.Request, m artichoke.Data) bool {
+				if m["bodyJson"] != nil {
+					writeError(w, 500, "DELETE requests with parameters is not supported yet")
+					return true
+				}
+
+				// create a new session per connection
+				s := session.New()
+				defer s.Close()
+
+				params := m["params"].(map[string]string)
+				err := s.DB(params["db"]).C(params["collection"]).DropCollection()
+				if err != nil {
+					writeError(w, 500, "Error dropping collection")
+					return true
+				}
+
+				w.Header().Add("Content-Length", "0")
+				w.WriteHeader(204)
+				return true
+			},
+		},
+		artichoke.Route{
+			Method: "DELETE",
+			Pattern: "/:db",
+			Handler: func(w http.ResponseWriter, r *http.Request, m artichoke.Data) bool {
+				if m["bodyJson"] != nil {
+					writeError(w, 500, "DELETE requests with parameters is not supported yet")
+					return true
+				}
+
+				// create a new session per connection
+				s := session.New()
+				defer s.Close()
+
+				params := m["params"].(map[string]string)
+				err := s.DB(params["db"]).DropDatabase()
+				if err != nil {
+					writeError(w, 500, "Error dropping database")
+					return true
+				}
+
+				w.Header().Add("Content-Length", "0")
+				w.WriteHeader(204)
+				return true
+			},
+		},
+
+		/* PUT Requests */
+
+		// Since databases and collections are created on-the-fly, there's no PUT for a database or collection.
+		//
+		// A PUT request will update the URI (overwriting an existing resource), so Insert is used.
+		// Note that the document id is mandatory. For an auto-generated ID, POST to the collection instead.
+		//
+		// For updates, use POST instead (on an existing resource).
+		artichoke.Route{
+			Method: "PUT",
+			Pattern: "/:db/:collection/:docid",
+			Handler: func(w http.ResponseWriter, r *http.Request, m artichoke.Data) bool {
+				// create a new session per connection
+				s := session.New()
+				defer s.Close()
+
+				// get the params and query objects
+				params := m["params"].(map[string]string)
+				query := m["query"].(url.Values)
+
+				var id interface{}
+				if query.Get("hexId") == "false" {
+					id = params["docid"]
+				} else {
+					str, err := hex.DecodeString(params["docid"])
+					if err != nil {
+						writeError(w, 500, "Unable to convert supplied string to hex format")
+						return true
+					} else if !bson.ObjectId(str).Valid() {
+						writeError(w, 400, "Invalid hex string. Maybe you meant to pass the query param hexId=false?")
+						return true
+					}
+
+					id = bson.ObjectId(str)
+				}
+
+				// get the collection so we can check if the id exists
+				c := s.DB(params["db"]).C(params["collection"])
+
+				// try finding as hex
+				n, err := c.Find(bson.M{"_id": id}).Count()
+				if err != nil {
+					writeError(w, 500, "Error communicating with database")
+					return true
+				}
+
+				// put the id in
+				body := m["bodyJson"].(map[string]interface{})
+				body["_id"] = id
+
+				// insert into the database
+				err = c.Insert(body)
+				if err != nil {
+					writeError(w, 500, "Error inserting document into database")
+					return true
+				}
+
+				w.Header().Add("Content-Length", "0")
+				if n > 0 {
+					w.WriteHeader(204)
+					return true
+				} else {
+					w.WriteHeader(201)
+				}
+
+				return true
+			},
+		},
 	};
 }
 
@@ -157,7 +372,7 @@ func main() {
 		panic(err)
 	}
 
-	server := artichoke.New(nil, artichoke.BodyParser(1024), artichoke.Router(genRoutes()))
+	server := artichoke.New(nil, artichoke.QueryParser(), artichoke.BodyParser(1024), artichoke.Router(genRoutes()))
 	server.Run(3345, "localhost")
 
 	<-kill
